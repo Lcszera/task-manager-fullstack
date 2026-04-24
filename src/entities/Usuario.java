9a5450c7ba370ece97b80cfcee0c32568c9799ae
LUCAS MENEZES SILVA <lucas.menezes@seplag.al>
2026-04-24 10:10:02 -0300

    initial commit

---
+++ src/entities/Usuario.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Usuario {
+
+    private String nome;
+    private String orgao;
+    private int senha;
+
+    public Usuario (String nome, String orgao, int senha) {
+
+        this.nome = nome;
+        this.orgao = orgao;
+        this.senha = senha;
+
+    }
+
+}
+
+
