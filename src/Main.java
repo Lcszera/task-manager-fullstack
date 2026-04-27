1234c03651cfb853df3f4754e41ebc9073218706
Lucas Menezes Silva <lucasmen2005@gmail.com>
2026-04-27 20:21:59 -0300

    feat: implementa UsuarioService com cadastro e login e integração na Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import entities.Usuario;
 import entities.Tarefa;
+import services.UsuarioService;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +9,19 @@ public class Main {
 
         Tarefa tarefa = new Tarefa(1, "Estudar Java", "POO e Git", 3);
 
-        System.out.println("Usuário: " + user.getNome());
+        UsuarioService service = new UsuarioService();
+
+        service.cadastrar("Lucas", "lucasmen2005@gmail.com", "lucas123");
+        service.cadastrar("Rafaela", "rafalouyse@gmail.com", "rafinha123");
+
+        Usuario logado = service.login("lucasmen2005@gmail.com", "lucas123");
+
+        if (logado != null) {
+            System.out.println("Login realizado: " + logado.getNome());
+        } else {
+            System.out.println("Email ou senha inválidos");
+        }
+
         System.out.println("Tarefa: " + tarefa.getTitulo());
 
         tarefa.concluir();
